04081bcb38e19c5249ad548cb5226595d51a0e26
Maanastej <143927039+Maanastej@users.noreply.github.com>
2026-02-24 09:01:40 +0530

    Add files via upload

---
+++ Health Synergy/code-02.js
@@ -0,0 +1,20 @@
+console.log("hello")
+console.log(3)
+console.log
+let x;
+x="kdcmdksc"
+console.log(x)
+
+
+let username;
+document.getElementById("submitbutton").onclick= function(){
+    username=document.getElementById("username").value;
+    document.getElementById("q1").innerHTML = username;
+
+}
+let D;
+D = window.prompt("enter a number");
+a=Number(D);
+console.log(D);
+
+
